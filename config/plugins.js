35fc6804ba5eb70639a40ed906e00c641e479190
John Doe <johndoe@email.com>
2023-12-14 13:51:30 +0530

    first upload

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: 'dh0tn7tyj',
+          api_key: '745852767248875',
+          api_secret: 'zczixaHiwvIQC0Dzw85FqI_09oQ',
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
